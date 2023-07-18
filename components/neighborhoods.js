819f119f90dffb6fed81717d7e0994d727ebd033
abhinash tummala <abhinash.t12@gmail.com>
2023-07-18 15:24:57 -0700

    added shadcn ui compoents and added neighborhod buttons on top , added mode toggle

---
+++ components/neighborhoods.js
@@ -0,0 +1,27 @@
+export const Neighborhoods = {
+  downtown: {
+    lat: "47.61195",
+    lng: "-122.33860",
+    name: "Downtown",
+  },
+  ballard: {
+    lat: "47.66873",
+    lng: "-122.38217",
+    name: "Ballard",
+  },
+  greenlake: {
+    lat: "47.68587",
+    lng: "-122.33743",
+    name: "Greenlake",
+  },
+  freemont: {
+    lat: "47.65165",
+    lng: "-122.35175",
+    name: "Fremont",
+  },
+  caphill: {
+    lat: "47.61411",
+    lng: "-122.31691",
+    name: "Capitol Hill",
+  },
+};
